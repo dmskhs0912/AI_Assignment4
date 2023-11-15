41919ddb3f07523f347c12de46da6479a0d42fe7
unknown <dmskhs0912@gmail.com>
2023-11-16 01:26:41 +0900

    Probability v1.1

--- hw4.py
+++ hw4.py
@@ -2,17 +2,6 @@ import numpy as np
 
 
 def joint_distribution_of_word_counts(texts, word0, word1):
-    """
-    Parameters:
-    texts (list of lists) - a list of texts; each text is a list of words
-    word0 (str) - the first word to count
-    word1 (str) - the second word to count
-
-    Output:
-    Pjoint (numpy array) - Pjoint[m,n] = P(X0=m,X1=n), where
-      X0 is the number of times that word0 occurs in a given text,
-      X1 is the number of times that word1 occurs in the same text.
-    """
     # 단어 카운트를 위한 최대값 초기화
     max_count_word0 = 0
     max_count_word1 = 0
@@ -38,11 +27,11 @@ def joint_distribution_of_word_counts(texts, word0, word1):
     # Pjoint 배열 초기화
     Pjoint = np.zeros((max_count_word0 + 1, max_count_word1 + 1))
 
-    # 결합 분포 계산
+    # Pjoint[m, n] 모든 m, n에 대해 경우의 수 계산
     for count_word0, count_word1 in zip(counts_word0, counts_word1):
         Pjoint[count_word0, count_word1] += 1
 
-    # 정규화
+    # 전체 경우의 수로 나눠 확률 계산
     Pjoint /= np.sum(Pjoint)
 
     return Pjoint
@@ -50,19 +39,6 @@ def joint_distribution_of_word_counts(texts, word0, word1):
 
 
 def marginal_distribution_of_word_counts(Pjoint, index):
-    """
-    Parameters:
-    Pjoint (numpy array) - Pjoint[m,n] = P(X0=m,X1=n), where
-      X0 is the number of times that word0 occurs in a given text,
-      X1 is the number of times that word1 occurs in the same text.
-    index (0 or 1) - which variable to retain (marginalize the other)
-
-    Output:
-    Pmarginal (numpy array) - Pmarginal[x] = P(X=x), where
-      if index==0, then X is X0
-      if index==1, then X is X1
-    """
-    # 주변 확률 분포 계산
     if index == 0:
         # index가 0이면, 각 행의 합을 계산
         Pmarginal = np.sum(Pjoint, axis=1)
@@ -77,53 +53,31 @@ def marginal_distribution_of_word_counts(Pjoint, index):
 
 
 def conditional_distribution_of_word_counts(Pjoint, Pmarginal):
-    """
-    Parameters:
-    Pjoint (numpy array) - Pjoint[m,n] = P(X0=m,X1=n), where
-      X0 is the number of times that word0 occurs in a given text,
-      X1 is the number of times that word1 occurs in the same text.
-    Pmarginal (numpy array) - Pmarginal[m] = P(X0=m)
-
-    Outputs:
-    Pcond (numpy array) - Pcond[m,n] = P(X1=n|X0=m)
-    """
-    # Pcond 배열 초기화
+    # Pcond 배열 Pjoint와 동일한 형태로 초기화.
     Pcond = np.zeros_like(Pjoint)
 
     # 조건부 확률 계산
-    for m in range(Pjoint.shape[0]):
-        for n in range(Pjoint.shape[1]):
+    for m in range(Pjoint.shape[0]): # 행의 개수만큼 반복
+        for n in range(Pjoint.shape[1]): # 열의 개수만큼 반복
             if Pmarginal[m] > 0:
                 Pcond[m, n] = Pjoint[m, n] / Pmarginal[m]
             else:
-                # Pmarginal[m]이 0인 경우 Pcond[m, n]을 NaN으로 설정
+                # 분모가 0인 경우 Pcond[m, n]을 NaN으로 설정
                 Pcond[m, n] = np.nan
 
     return Pcond
 
 def mean_from_distribution(P):
-    """
-    Parameters:
-    P (numpy array) - P[n] = P(X=n)
 
-    Outputs:
-    mu (float) - the mean of X
-    """
-    # 확률 분포와 인덱스를 곱하여 기대값을 계산
-    n = np.arange(len(P))
+    # P의 원소 개수 k개일 때, n = [0, 1, 2, ..., k-1]
+    n = np.arange(len(P)) 
+    # n, P 두 배열 각각 원소별로 곱해 더한 후 반올림
     mu = round(np.sum(n * P), 3)
     
     return mu
 
 
 def variance_from_distribution(P):
-    """
-    Parameters:
-    P (numpy array) - P[n] = P(X=n)
-
-    Outputs:
-    var (float) - the variance of X
-    """
     # 평균 계산
     n = np.arange(len(P))
     mu = np.sum(n * P)
@@ -135,17 +89,11 @@ def variance_from_distribution(P):
 
 
 def covariance_from_distribution(P):
-    """
-    Parameters:
-    P (numpy array) - P[m,n] = P(X0=m,X1=n)
-
-    Outputs:
-    covar (float) - the covariance of X0 and X1
-    """
-    # 각 변수에 대한 평균 계산
+    # P를 행 별로 잘라서 m에, 열 별로 잘라서 n에 저장
     m, n = np.indices(P.shape)
     mu_m = np.sum(m * P)
     mu_n = np.sum(n * P)
+
     
     # 공분산 계산
     covar = round(np.sum((m - mu_m) * (n - mu_n) * P), 3)
@@ -153,18 +101,9 @@ def covariance_from_distribution(P):
     return covar
 
 def expectation_of_a_function(P, f):
-    """
-    Parameters:
-    P (numpy array) - joint distribution, P[m,n] = P(X0=m,X1=n)
-    f (function) - f should be a function that takes two
-       real-valued inputs, x0 and x1.  The output, z=f(x0,x1),
-       must be a real number for all values of (x0,x1)
-       such that P(X0=x0,X1=x1) is nonzero.
-
-    Output:
-    expected (float) - the expected value, E[f(X0,X1)]
-    """
+    # P를 행 별로 잘라서 m에, 열 별로 잘라서 n에 저장
     m, n = np.indices(P.shape)
+    # f(m,n) 값에 대해 평균 계산
     expected = round(np.sum(f(m, n) * P), 3)
     
     return expected
